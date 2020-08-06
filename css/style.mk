af95dd9147a6ed4e8debc51b3d7754f780985d30
sangeethkumar2004 <69237762+sangeethkumar2004@users.noreply.github.com>
2020-08-06 17:29:54 +0530

    Add files via upload

---
+++ css/style.mk
@@ -0,0 +1,16 @@
+#topbar
+{
+background-color: #1F1F1F;
+height:10px;
+width:100%;
+color:red;
+}
+body
+{
+background-color:white; 
+}
+#container
+{
+background-colour:red;
+height:700px
+width:100% 
